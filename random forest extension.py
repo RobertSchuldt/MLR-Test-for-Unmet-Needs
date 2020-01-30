e764be67ec3d0e3e569c0369682832656edecab0
RobertSchuldt <45829365+RobertSchuldt@users.noreply.github.com>
2020-01-30 10:06:49 -0600

    Changes to file
    Now random forest is better functioning with K fold cross validation

--- random forest extension.py
+++ random forest extension.py
@@ -1,95 +1,145 @@
 # -*- coding: utf-8 -*-
-
-
 """
-Created on Tue Aug 20 11:39:32 2019
+Created on Wed Jan 29 10:34:39 2020
 
-@author: Robert Schuldtd
-
-SKLEARN Practice for Random Forest using my outcome and the unmet 
-caregiver needs. Test for using in dissertation and feature selection
-to properly weight the dimensions of unmet caregiver needs in the study
+@author: 3043340
 """
 
-import pandas as pd
-import sklearn as sk
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 24 10:28:01 2020
 
-#load my data set
-data = pd.read_csv("****randomforest2.csv", header = 0)
+@author: Robert Schuldt
 
-#check variable names
+MLR Model Random Forest
+"""
 
-var_list = data.head()
 
-data = data[['outcomes', 'un_adl', 'un_iadl', 'un_mdctn', 'un_equip', 'un_prcdr', 'un_sprvsn', 'un_advcy', 'post_acute_pat', 'dual', 'female', 'white', 
-             'black', 'hispanic', 'other_race', 'p_sever_mid', 'nd_sever_high', 'nutrition', 'stable', 'risk', 'lives_with', 'alone_ass', 'pain1', 'pain2',
-             'ulcer2_up', 'surg_wd_lesion', 'dyspenic', 'respritory', 'uti', 'u_incntn', 'cog_fun_high', 'depression_mid', 'depression_high' , 'groom',
-             'dress_up' , 'dress_down', 'bath', 'toliet' , 'hygiene', 'transfer', 'ambu', 'feeding', 'fall_risk' ]]
 
-from sklearn.model_selection import train_test_split
-
-X = data[[ 'un_adl', 'un_iadl', 'un_mdctn', 'un_equip', 'un_prcdr', 'un_sprvsn', 'un_advcy', 'post_acute_pat', 'dual', 'female', 'white', 
-             'black', 'hispanic', 'other_race', 'p_sever_mid', 'nd_sever_high', 'nutrition', 'stable', 'risk', 'lives_with', 'alone_ass', 'pain1', 'pain2',
-             'ulcer2_up', 'surg_wd_lesion', 'dyspenic', 'respritory', 'uti', 'u_incntn', 'cog_fun_high', 'depression_mid', 'depression_high' , 'groom',
-             'dress_up' , 'dress_down', 'bath', 'toliet' , 'hygiene', 'transfer', 'ambu', 'feeding', 'fall_risk' ]] 
-X = X.fillna(0)
-#name my features and name my classes
-feature_names = ['Unmet ADL', 'Unmet IADL', 'Unmet Medication', 'Unmet Equipment', 'Unmet Procedure', 'Unmet Supervision', 'Unmet Advocacy''post_acute_pat', 'dual', 'female', 'white', 
-             'black', 'hispanic', 'other_race', 'p_sever_mid', 'nd_sever_high', 'nutrition', 'stable', 'risk', 'lives_with', 'alone_ass', 'pain1', 'pain2',
-             'ulcer2_up', 'surg_wd_lesion', 'dyspenic', 'respritory', 'uti', 'u_incntn', 'cog_fun_high', 'depression_mid', 'depression_high' , 'groom',
-             'dress_up' , 'dress_down', 'bath', 'toliet' , 'hygiene', 'transfer', 'ambu', 'feeding', 'fall_risk' ] 
-target_names = ['No_bad','hosp_stay', 'snf_stay','long_stay', 'death']
-#Outcome variable 
-y= data[['outcomes']]
-
-#Split data set into test and train
-#Set 70% to train and 30% to test
-X_Train, X_test, y_train, y_test = train_test_split(X,y, test_size = 0.3)
-
-#Import my classifier
-
+import pandas as pd 
+import numpy as np
+from sklearn.metrics import accuracy_score, confusion_matrix
 from sklearn.ensemble import RandomForestClassifier
 
-#create my Gaussian Classifier
-clf=RandomForestClassifier(bootstrap=True, class_weight=None , criterion='gini',
-            max_depth=None, max_features='auto', max_leaf_nodes=None,
-            min_impurity_decrease=0.0, min_impurity_split=None,
-            min_samples_leaf=1, min_samples_split=2,
-            min_weight_fraction_leaf=0.0, n_estimators=100, n_jobs=1,
-            oob_score=False, random_state=None, verbose=0,
-            warm_start=False)
+from sklearn.model_selection import train_test_split
+import seaborn as sns
 
-clf.fit(X_Train, y_train.values.ravel())
+csv_file = "*****\Python\\MLR Testing2.csv"
+c_size = 50000
 
-y_pred=clf.predict(X_test)
+data_set = pd.DataFrame()
 
+for gm_chunk in pd.read_csv(csv_file, chunksize=c_size, header= 0):
+    data_set = data_set.append(gm_chunk)
+    print(gm_chunk.shape)
+   
 
-feature_imp = pd.Series(clf.feature_importances_ ,index= feature_names).sort_values(ascending=False)
-
-from sklearn.metrics import confusion_matrix
-cf = confusion_matrix(y_test, y_pred)
-print("Confusion Matrix:" , cf)
-
-from sklearn import metrics
-
-print("Accuracy:", metrics.accuracy_score(y_test, y_pred))
+    
+view = data_set.describe()    
 
 
+
+data_set = data_set
+
+target_names = ['No_bad', 'Hospital','LTC']
+
+X = data_set.drop(columns =['outcome', 'STATE_CODE', 'COUNTY_CD', 'fips_state',
+                             'any_hosp_stay', 'M0010_MEDICARE_ID', 'M0030_SOC_DT', 
+                             'M1020_PRI_DGN_ICD', 'nutrition', 'age_mark', 'ffs',
+                             'inpat_event_pre', 'Employment_Rate_16_', 'Education_Level',
+                             'death_30', 'death_90', 'index' ])
+
+varlist = X.describe()
+    
+feature_names= list(X.columns.values)
+
+y = data_set[['outcome']]
+
+
+y.describe()
+id_y = y['outcome'].value_counts()
+# Split the data set into test and training'''
+X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.3, shuffle=True)
+
+
+#Adjust sampling b/c we have imbalanced Data set
+from imblearn.over_sampling import SMOTE
+
+smote= SMOTE('not majority', random_state = 42)
+X_sm, y_sm =  smote.fit_sample(X_train, y_train)
+
+from sklearn.model_selection import RandomizedSearchCV
+# Number of trees in random forest
+n_estimators = [int(x) for x in np.linspace(start = 200, stop = 2000, num = 10)]
+# Number of features to consider at every split
+max_features = ['auto', 'sqrt']
+# Maximum number of levels in tree
+max_depth = [int(x) for x in np.linspace(10, 110, num = 11)]
+max_depth.append(None)
+# Minimum number of samples required to split a node
+min_samples_split = [2, 5, 10]
+# Minimum number of samples required at each leaf node
+min_samples_leaf = [1, 2, 4]
+# Method of selecting samples for training each tree
+bootstrap = [True, False]
+# Create the random grid
+random_grid = {'n_estimators': n_estimators,
+               'max_features': max_features,
+               'max_depth': max_depth,
+               'min_samples_split': min_samples_split,
+               'min_samples_leaf': min_samples_leaf,
+               'bootstrap': bootstrap}
+
+
+#Want to place my acc and feature weights into an empy
+
+model_acc = []
+
+feature_meaures = []
+
+#Now to apply my randomized grid to the random forest classifer
+clf = RandomForestClassifier()
+
+rf_random = RandomizedSearchCV(estimator = clf, param_distributions = random_grid, n_iter = 100, cv = 3, verbose=2, random_state=42, n_jobs = 4)
+# Fit the random search model
+rf_random.fit(X_sm, y_sm)
+
+train_pred= clf.predict(X_sm)
+y_pred= clf.predict(X_test)
+acc = accuracy_score(y_test, y_pred)
+model_acc.append(acc)
+print("Accuracy of %s is %s"%(clf, acc))
+cm = confusion_matrix(y_test, y_pred)
+print("Confusion Matrix of %s is %s"%(clf, cm))
+
+#Create my feature importance matrix 
+        
+        
+feature_imp = pd.Series(clf.feature_importances_ , index= feature_names).sort_values(ascending=False)
+feature_meaures.append(feature_imp)
 import matplotlib.pyplot as plt
-import seaborn as sns 
-
-
+            
+        
 #create a bar plot of features
-
+        
 feature_plot = sns.barplot(x=feature_imp, y=feature_imp.index)
 #add labels
-
+            
 plt.xlabel('Feature Importance Score')
 plt.ylabel('Features')
+plt.tick_params(axis= 'y', which= 'major', pad = 10)
 plt.title("Visualizing Important Features")
 plt.legend
+plt.tight_layout()
 plt.show(feature_plot)
-feature_plot.figure.savefig('Z:\\DATA\\Dissertation\\Python\\feature_importance.png')
+plt.figure(figsize = (12, 12))
+plt.rcParams["ytick.labelsize"] = 3
+feature_plot.figure.savefig('Z:\\DATA\\Dissertation\\Python\\feature_importance.png', dpi = 300)
+
+best_fit = rf_random.best_params_
+
+
+
 
 #visualize confusion Matrix
 def plot_confusion_matrix(cm, 
@@ -99,7 +149,6 @@ def plot_confusion_matrix(cm,
                           normalize = True):
     
     import matplotlib.pyplot as plt
-    import numpy as np
     import itertools
     
     accuracy = np.trace(cm) /float(np.sum(cm))
@@ -107,21 +156,20 @@ def plot_confusion_matrix(cm,
     
     if cmap is None:
         cmap = plt.get_cmap('Blues')
-    
         plt.figure(figsize=(8, 6))
-    plt.imshow(cm, interpolation='nearest', cmap=cmap)
-    plt.title(title)
-    plt.colorbar()
-
+        plt.imshow(cm, interpolation='nearest', cmap=cmap)
+        plt.title(title)
+        plt.colorbar()
+    
     if target_names is not None:
         tick_marks = np.arange(len(target_names))
         plt.xticks(tick_marks, target_names, rotation=45)
         plt.yticks(tick_marks, target_names)
-
+    
     if normalize:
         cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
-
-
+    
+    
     thresh = cm.max() / 1.5 if normalize else cm.max() / 2
     for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
         if normalize:
@@ -132,15 +180,19 @@ def plot_confusion_matrix(cm,
             plt.text(j, i, "{:,}".format(cm[i, j]),
                      horizontalalignment="center",
                      color="white" if cm[i, j] > thresh else "black")
-
- 
+    
+    
     plt.tight_layout()
     plt.ylabel('True label')
     plt.xlabel('Predicted label\naccuracy={:0.4f}; misclass={:0.4f}'.format(accuracy, misclass))
     plt.show()
 
-    
-plot_confusion_matrix(cm = cf,
+
+
+plot_confusion_matrix(cm = cm,
                       normalize = False,
-                      target_names = target_names,
-                      title = 'Confusion Matrix')    
+                      target_names = None,
+                      title = 'Confusion Matrix')
+
+
+
--- random forest.py
+++ random forest.py
@@ -1,83 +1,127 @@
 # -*- coding: utf-8 -*-
-
-
 """
-Created on Tue Aug 20 11:39:32 2019
+Created on Fri Jan 24 10:28:01 2020
 
-@author: Robert Schuldtd
+@author: Robert Schuldt
 
-SKLEARN Practice for Random Forest using my outcome and the unmet 
-caregiver needs. Test for using in dissertation and feature selection
-to properly weight the dimensions of unmet caregiver needs in the study
+MLR Model Random Forest
 """
 
-import pandas as pd
-import sklearn as sk
 
-#load my data set
-data = pd.read_csv(*******\randomforest.csv", header = 0)
-
-#check variable names
-
-var_list = data.head()
-
-data = data[['outcomes', 'un_adl', 'un_iadl', 'un_mdctn', 'un_equip', 'un_prcdr', 'un_sprvsn', 'un_advcy', 'post_acute_pat' ]]
-
-from sklearn.model_selection import train_test_split
-
-X = data[[ 'un_adl', 'un_iadl', 'un_mdctn', 'un_equip', 'un_prcdr', 'un_sprvsn', 'un_advcy']] 
-#name my features and name my classes
-feature_names = ['Unmet ADL', 'Unmet IADL', 'Unmet Medication', 'Unmet Equipment', 'Unmet Procedure', 'Unmet Supervision', 'Unmet Advocacy']
-target_names = ['No_bad','hosp_stay', 'snf_stay','long_stay', 'death' ]
-#Outcome variable 
-y= data[['outcomes']]
-
-#Split data set into test and train
-#Set 70% to train and 30% to test
-X_Train, X_test, y_train, y_test = train_test_split(X,y, test_size = 0.3)
-
-#Import my classifier
-
+import pandas as pd 
+import numpy as np
+from sklearn.metrics import accuracy_score, confusion_matrix
 from sklearn.ensemble import RandomForestClassifier
 
-#create my Gaussian Classifier
-clf=RandomForestClassifier(bootstrap=True, class_weight=None , criterion='gini',
-            max_depth=None, max_features='auto', max_leaf_nodes=None,
-            min_impurity_decrease=0.0, min_impurity_split=None,
-            min_samples_leaf=1, min_samples_split=2,
-            min_weight_fraction_leaf=0.0, n_estimators=100, n_jobs=1,
-            oob_score=False, random_state=None, verbose=0,
-            warm_start=False)
+from sklearn.model_selection import train_test_split
+import seaborn as sns
 
-clf.fit(X_Train, y_train.values.ravel())
+csv_file = "*****\\Python\\MLR Testing2.csv"
+c_size = 50000
 
-y_pred=clf.predict(X_test)
+data_set = pd.DataFrame()
 
+for gm_chunk in pd.read_csv(csv_file, chunksize=c_size, header= 0):
+    data_set = data_set.append(gm_chunk)
+    print(gm_chunk.shape)
+   
 
-feature_imp = pd.Series(clf.feature_importances_ ,index= feature_names).sort_values(ascending=False)
-
-from sklearn.metrics import confusion_matrix
-cf = confusion_matrix(y_test, y_pred)
-print("Confusion Matrix:" , cf)
-
-from sklearn import metrics
-
-print("Accuracy:", metrics.accuracy_score(y_test, y_pred))
+    
+view = data_set.describe()    
 
 
-import matplotlib.pyplot as plt
-import seaborn as sns 
+
+data_set = data_set
+
+target_names = ['No_bad', 'Hospital','LTC']
+
+X = data_set.drop(columns =['outcome', 'STATE_CODE', 'COUNTY_CD', 'fips_state',
+                             'any_hosp_stay', 'M0010_MEDICARE_ID', 'M0030_SOC_DT', 
+                             'M1020_PRI_DGN_ICD', 'nutrition', 'age_mark', 'ffs',
+                             'inpat_event_pre', 'Employment_Rate_16_', 'Education_Level',
+                             'death_30', 'death_90', 'index' ])
+
+varlist = X.describe()
+    
+feature_names= list(X.columns.values)
+
+y = data_set[['outcome']]
 
 
-#create a bar plot of features
+y.describe()
+id_y = y['outcome'].value_counts()
+# Split the data set into test and training'''
+X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.3, shuffle=True)
 
-sns.barplot(x=feature_imp, y=feature_imp.index)
-#add labels
-plt.xlabel('Feature Importance Score')
-plt.ylabel('Features')
-plt.title("Visualizing Important Features")
-plt.legend
-plt.show()
+
+#Adjust sampling b/c we have imbalanced Data set
+from imblearn.over_sampling import SMOTE
+
+smote= SMOTE('not majority', random_state = 42)
+X_sm, y_sm =  smote.fit_sample(X_train, y_train)
+
+#create graphics and vars for testing best selection of criteria for hyperparms
+n_estimators = [ 30, 50, 66, 68, 75, 78, 80, 87, 90, 100, 115]
+
+splits = [ 0.1, .15, .2 ]
+leaves = [2, 4, 5, 7]
+
+model_acc = []
+
+feature_meaures = []
+for estimators  in n_estimators:
+    for x in splits:
+        for y in leaves:
+                #Build random forest classifier
+                clf = RandomForestClassifier(n_estimators = estimators, 
+                                             random_state = 42,
+                                             verbose = 1,
+                                             max_features = 'sqrt',
+                                             min_samples_split = x ,
+                                             min_samples_leaf = y,
+                                             criterion = 'gini',
+                                             oob_score = True,
+                                             n_jobs = 1,
+                                             max_depth = None,
+                                             
+                                                                         
+                                             )
+                clf.fit(X_sm, y_sm)
+                train_pred= clf.predict(X_sm)
+                y_pred= clf.predict(X_test)
+                acc = accuracy_score(y_test, y_pred)
+                model_acc.append(acc)
+                print("Accuracy of %s is %s"%(clf, acc))
+                cm = confusion_matrix(y_test, y_pred)
+                print("Confusion Matrix of %s is %s"%(clf, cm))
+            
+   
+#Create my feature importance matrix 
+        
+        
+                feature_imp = pd.Series(clf.feature_importances_ , index= feature_names).sort_values(ascending=False)
+                feature_meaures.append(feature_imp)
+                import matplotlib.pyplot as plt
+            
+        
+            #create a bar plot of features
+        
+                feature_plot = sns.barplot(x=feature_imp, y=feature_imp.index)
+            #add labels
+            
+                plt.xlabel('Feature Importance Score')
+                plt.ylabel('Features')
+                plt.tick_params(axis= 'y', which= 'major', pad = 10)
+                plt.title("Visualizing Important Features")
+                plt.legend
+                plt.tight_layout()
+                plt.show(feature_plot)
+                plt.figure(figsize = (12, 12))
+                plt.rcParams["ytick.labelsize"] = 3
+                feature_plot.figure.savefig('*******\\Python\\feature_importance'+str(estimators)+'.png', dpi = 300)
+
+
+
 
 #visualize confusion Matrix
 def plot_confusion_matrix(cm, 
@@ -87,7 +131,6 @@ def plot_confusion_matrix(cm,
                           normalize = True):
     
     import matplotlib.pyplot as plt
-    import numpy as np
     import itertools
     
     accuracy = np.trace(cm) /float(np.sum(cm))
@@ -95,21 +138,20 @@ def plot_confusion_matrix(cm,
     
     if cmap is None:
         cmap = plt.get_cmap('Blues')
-    
         plt.figure(figsize=(8, 6))
-    plt.imshow(cm, interpolation='nearest', cmap=cmap)
-    plt.title(title)
-    plt.colorbar()
-
+        plt.imshow(cm, interpolation='nearest', cmap=cmap)
+        plt.title(title)
+        plt.colorbar()
+    
     if target_names is not None:
         tick_marks = np.arange(len(target_names))
         plt.xticks(tick_marks, target_names, rotation=45)
         plt.yticks(tick_marks, target_names)
-
+    
     if normalize:
         cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
-
-
+    
+    
     thresh = cm.max() / 1.5 if normalize else cm.max() / 2
     for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
         if normalize:
@@ -120,14 +162,28 @@ def plot_confusion_matrix(cm,
             plt.text(j, i, "{:,}".format(cm[i, j]),
                      horizontalalignment="center",
                      color="white" if cm[i, j] > thresh else "black")
-
-
+    
+    
     plt.tight_layout()
     plt.ylabel('True label')
     plt.xlabel('Predicted label\naccuracy={:0.4f}; misclass={:0.4f}'.format(accuracy, misclass))
     plt.show()
-    
-plot_confusion_matrix(cm = cf,
+
+
+
+plot_confusion_matrix(cm = cm,
                       normalize = False,
-                      target_names = target_names,
-                      title = 'Confusion Matrix')    
+                      target_names = None,
+                      title = 'Confusion Matrix')
+
+
+#Need to do k-fold cross validation on the final model that I have selected to 
+#to evaluate the 
+
+from sklearn.model_selection import cross_val_score
+
+score_rf = cross_val_score(clf, X, y)
+print(score_rf)
+
+
+ 
